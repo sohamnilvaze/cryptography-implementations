18be081f337462166ff5cfa5e8e56fad779d5aaa
sohamnilvaze <sohamnilvaze.39@gmail.com>
2025-10-22 20:42:38 +0530

    Finished md5 implementation

--- md5.cpp
+++ md5.cpp
@@ -7,18 +7,25 @@
 #include <cmath>
 #include <iomanip>
 #include <sstream>
+#include <bitset>
+#include <cstdint>
 using namespace std;
 
-
-#define A 0x67452301
-#define B 0xefcdab89
-#define C 0x98badcfe
-#define D 0x10325476
 
 #define F(b,c,d) (b & c) | (!b & d) 
 #define G(b,c,d) (b & d) | (c & !d)
 #define H(b,c,d) (b ^ c ^ c)
 #define I(b,c,d) (c ^ (b | !d))
+
+uint32_t A = 0x67452301;
+uint32_t B = 0xefcdab89;
+uint32_t C = 0x98badcfe;
+uint32_t D = 0x10325476;
+
+string to_binary_string(unsigned int value) {
+    bitset<32> bits(value);
+    return bits.to_string();  // returns std::string of length 32
+}
 
 string dec_to_binary(uint64_t num)
 {
@@ -265,10 +272,264 @@ vector<vector<string>> bin_str_to_hex(vector<vector<char>> bin_str)
     return res;
 }
 
+uint32_t bits_to_uint32(vector<char>& bits) {
+    if (bits.size() != 32) {
+        throw std::invalid_argument("Expected exactly 32 bits");
+    }
+    
+    uint32_t result = 0;
+    for (size_t i = 0; i < 32; ++i) {
+        if (bits[i] == '1') {
+            result |= (1u << (31 - i)); // MSB first
+        } 
+        else if (bits[i] != '0') {
+            throw invalid_argument("Bits must be '0' or '1'");
+        }
+    }
+    return result;
+}
+
+vector<uint32_t> first_round(vector<vector<char>> padded,uint32_t A, uint32_t B, uint32_t C, uint32_t D)
+{
+    vector<uint32_t> ks = {0xD76AA478, 0xE8C7B756, 0x242070DB, 0xC1BDCEEE, 0xF57C0FAF, 0x4787C62A, 0xA8304613, 0xFD469501, 0x698098D8, 0x8B44F7AF, 0xFFFF5BB1, 0x895CD7BE, 0x6B901122, 0xFD9886BA, 0xAEF127FA, 0x4292244};
+    // 1,5,9,13 = 7;
+    // 2,6,10,14 = 12;
+    // 3,7,11,15 = 17;
+    // 4,8,12,16 = 22;
+    for(int i = 0; i<padded.size();i++)
+    {
+        uint32_t f_bcd = F(B,C,D);
+        uint32_t f_plus_a = f_bcd + A;
+        uint32_t m_con = bits_to_uint32(padded[i]);
+        uint32_t after_m = f_plus_a + m_con;
+        uint32_t after_k =  after_m + ks[i];
+        uint32_t after_shift;
+        if((i+1) % 4 == 1)
+        {
+            after_shift = 7 << after_k;
+        }
+        else if((i+1) % 4 == 2)
+        {
+            after_shift = 12 << after_k;
+        }
+        else if((i+1) % 4 == 3)
+        {
+            after_shift = 17 << after_k;
+        }
+        else
+        {
+            after_shift = 22 << after_k;
+        }
+        
+        uint32_t after_b = after_shift + B;
+        uint32_t A_temp = A;
+        uint32_t B_temp = B;
+        uint32_t C_temp = C;
+        uint32_t D_temp = D;
+
+        B = after_b;
+        A = D_temp;
+        C = B_temp;
+        D = C_temp;
+
+        
+    }
+
+    vector<uint32_t> res;
+
+    res.push_back(A);
+    res.push_back(B);
+    res.push_back(C);
+    res.push_back(D);
+
+    return res;
+
+}
+
+
+vector<uint32_t> second_round(vector<vector<char>> padded,uint32_t A, uint32_t B, uint32_t C, uint32_t D)
+{
+    vector<uint32_t> ks = {0xF61E2562, 0xC040B340, 0x265E5A51, 0xE9B6C7AA, 0xD62F105D, 0x02441453, 0xD8A1E681, 0xE7D3FBC8, 0x21E1CDE6, 0xC33707D6, 0xF4D50D87, 0x455A14ED, 0xA9E3E905, 0xFCEFA3F8, 0x676F02D9, 0x8D2A4C8A};
+    // 1,5,9,13 = 5;
+    // 2,6,10,14 = 9;
+    // 3,7,11,15 = 14;
+    // 4,8,12,16 = 20;
+    for(int i = 0; i<padded.size();i++)
+    {
+        uint32_t g_bcd = G(B,C,D);
+        uint32_t g_plus_a = g_bcd + A;
+        uint32_t m_con = bits_to_uint32(padded[i]);
+        uint32_t after_m = g_plus_a + m_con;
+        uint32_t after_k =  after_m + ks[i];
+        uint32_t after_shift;
+        if((i+1) % 4 == 1)
+        {
+            after_shift = 5 << after_k;
+        }
+        else if((i+1) % 4 == 2)
+        {
+            after_shift = 9 << after_k;
+        }
+        else if((i+1) % 4 == 3)
+        {
+            after_shift = 14 << after_k;
+        }
+        else
+        {
+            after_shift = 20 << after_k;
+        }
+        
+        uint32_t after_b = after_shift + B;
+        uint32_t A_temp = A;
+        uint32_t B_temp = B;
+        uint32_t C_temp = C;
+        uint32_t D_temp = D;
+
+        B = after_b;
+        A = D_temp;
+        C = B_temp;
+        D = C_temp;
+
+        
+    }
+
+    vector<uint32_t> res;
+
+    res.push_back(A);
+    res.push_back(B);
+    res.push_back(C);
+    res.push_back(D);
+
+    return res;
+
+}
+
+
+vector<uint32_t> third_round(vector<vector<char>> padded,uint32_t A, uint32_t B, uint32_t C, uint32_t D)
+{
+    vector<uint32_t> ks = {0x85845700, 0x676F02D9, 0x8D2A4C8A, 0xFFFA3942, 0x8771F681, 0x6D9D6122, 0xFDE5380C, 0xA4BEEA44, 0x4BDECFA9, 0xF6BB4B60, 0xBEBFBC70, 0x289B7EC6, 0xEAA127FA, 0xD4EF3085, 0x0654D202, 0x2B8699E5};
+    // 1,5,9,13 = 4;
+    // 2,6,10,14 =1;
+    // 3,7,11,15 = 16;
+    // 4,8,12,16 = 13;
+    for(int i = 0; i<padded.size();i++)
+    {
+        uint32_t h_bcd = H(B,C,D);
+        uint32_t h_plus_a = h_bcd + A;
+        uint32_t m_con = bits_to_uint32(padded[i]);
+        uint32_t after_m = h_plus_a + m_con;
+        uint32_t after_k =  after_m + ks[i];
+        uint32_t after_shift;
+        if((i+1) % 4 == 1)
+        {
+            after_shift = 4 << after_k;
+        }
+        else if((i+1) % 4 == 2)
+        {
+            after_shift = 11 << after_k;
+        }
+        else if((i+1) % 4 == 3)
+        {
+            after_shift = 16 << after_k;
+        }
+        else
+        {
+            after_shift = 13 << after_k;
+        }
+        
+        uint32_t after_b = after_shift + B;
+        uint32_t A_temp = A;
+        uint32_t B_temp = B;
+        uint32_t C_temp = C;
+        uint32_t D_temp = D;
+
+        B = after_b;
+        A = D_temp;
+        C = B_temp;
+        D = C_temp;
+
+        
+    }
+
+    vector<uint32_t> res;
+
+    res.push_back(A);
+    res.push_back(B);
+    res.push_back(C);
+    res.push_back(D);
+
+    return res;
+
+}
+
+
+
+vector<uint32_t> fourth_round(vector<vector<char>> padded,uint32_t A, uint32_t B, uint32_t C, uint32_t D)
+{
+    vector<uint32_t> ks = {0xFFFAF361, 0xBEFA4FA4, 0x6B3F8785, 0x895CD7BE, 0x6B901122, 0xFD9886BA, 0xAEF127FA, 0xF4292244, 0x432AFF97, 0xAB9423A7, 0xFC93A039, 0x655B59C3, 0x8F0CCC92, 0xFFEFF47D, 0x85845700, 0x676F02D9};
+    // 1,5,9,13 = 6;
+    // 2,6,10,14 =10;
+    // 3,7,11,15 = 15;
+    // 4,8,12,16 = 21;
+    for(int i = 0; i<padded.size();i++)
+    {
+        uint32_t i_bcd = I(B,C,D);
+        uint32_t i_plus_a = i_bcd + A;
+        uint32_t m_con = bits_to_uint32(padded[i]);
+        uint32_t after_m = i_plus_a + m_con;
+        uint32_t after_k =  after_m + ks[i];
+        uint32_t after_shift;
+        if((i+1) % 4 == 1)
+        {
+            after_shift = 6 << after_k;
+        }
+        else if((i+1) % 4 == 2)
+        {
+            after_shift = 10 << after_k;
+        }
+        else if((i+1) % 4 == 3)
+        {
+            after_shift = 115 << after_k;
+        }
+        else
+        {
+            after_shift = 21 << after_k;
+        }
+        
+        uint32_t after_b = after_shift + B;
+        uint32_t A_temp = A;
+        uint32_t B_temp = B;
+        uint32_t C_temp = C;
+        uint32_t D_temp = D;
+
+        B = after_b;
+        A = D_temp;
+        C = B_temp;
+        D = C_temp;
+
+        
+    }
+
+    vector<uint32_t> res;
+
+    res.push_back(A);
+    res.push_back(B);
+    res.push_back(C);
+    res.push_back(D);
+
+    return res;
+
+}
+
+
+
+
+
+
 
 int main()
 {
-    string plaintext;
+    string plaintext,A_fin,B_fin,C_fin,D_fin;
     cout<<"Enter the plaintext:\n";
     getline(cin,plaintext);
 
@@ -296,6 +557,70 @@ int main()
     }
     
     vector<vector<string>> padded_to_hex = bin_str_to_hex(padding_str);
+    cout<<"Size of padded_to_hex:- " << padded_to_hex.size() << " || " << padded_to_hex[0].size() << "\n";
+
+    vector<uint32_t> first_round_res = first_round(padding_str,A,B,C,D);
+    cout<<"After first round:- \n";
+    uint32_t after_first_A = first_round_res[0];
+    uint32_t after_first_B = first_round_res[1];
+    uint32_t after_first_C = first_round_res[2];
+    uint32_t after_first_D = first_round_res[3];
+    cout<<"A:- " << after_first_A << "\n";
+    cout<<"B:- " << after_first_B << "\n";
+    cout<<"C:- " << after_first_C << "\n";
+    cout<<"D:- " << after_first_D << "\n";
+
+
+
+    vector<uint32_t> second_round_res = second_round(padding_str,after_first_A,after_first_B,after_first_C,after_first_D);
+    cout<<"After second round:- \n";
+    uint32_t after_second_A = second_round_res[0];
+    uint32_t after_second_B = second_round_res[1];
+    uint32_t after_second_C = second_round_res[2];
+    uint32_t after_second_D = second_round_res[3];
+    cout<<"A:- " << after_second_A << "\n";
+    cout<<"B:- " << after_second_B << "\n";
+    cout<<"C:- " << after_second_C << "\n";
+    cout<<"D:- " << after_second_D << "\n";
+
+
+    vector<uint32_t> third_round_res = third_round(padding_str,after_second_A,after_second_B,after_second_C,after_second_D);
+    cout<<"After third round:- \n";
+    uint32_t after_third_A = third_round_res[0];
+    uint32_t after_third_B = third_round_res[1];
+    uint32_t after_third_C = third_round_res[2];
+    uint32_t after_third_D = third_round_res[3];
+    cout<<"A:- " << after_third_A << "\n";
+    cout<<"B:- " << after_third_B << "\n";
+    cout<<"C:- " << after_third_C << "\n";
+    cout<<"D:- " << after_third_D << "\n";
+
+
+    vector<uint32_t> fourth_round_res = fourth_round(padding_str,after_third_A,after_third_B,after_third_C,after_third_D);
+    cout<<"After fourth round:- \n";
+    uint32_t after_fourth_A = fourth_round_res[0];
+    uint32_t after_fourth_B = fourth_round_res[1];
+    uint32_t after_fourth_C = fourth_round_res[2];
+    uint32_t after_fourth_D = fourth_round_res[3];
+    cout<<"A:- " << after_fourth_A << "\n";
+    cout<<"B:- " << after_fourth_B << "\n";
+    cout<<"C:- " << after_fourth_C << "\n";
+    cout<<"D:- " << after_fourth_D << "\n";
+
+
+    A_fin = to_binary_string(after_fourth_A);
+    B_fin = to_binary_string(after_fourth_B);
+    C_fin = to_binary_string(after_fourth_C);
+    D_fin = to_binary_string(after_fourth_D);
+
+    string final_md5_hash = A_fin + B_fin + C_fin + D_fin;
+
+    cout<<"Final 128 bit hash value is:- " << final_md5_hash << "\n";
+
+
+
+
+
 
     return 0;
 
